6be5691ec8ee6434b5056ea1c171a7cdca5249d4
Jorge-Carvallho <138339721+Jorge-Carvallho@users.noreply.github.com>
2023-07-26 19:56:46 -0300

    Primeiros Exercícios do modulo java script I

---
+++ Calculador.js
@@ -0,0 +1,33 @@
+const x = parseFloat(prompt(" Informe o primeiro número: "));
+const y = parseFloat(prompt(" Informe o segundo número: "));
+
+// tambem pode ser feito desta forma
+/*
+const entrada1= prompt(' Informe o primeiro número')
+const entrada2= prompt('Infome o segundo número')
+
+**informando a constante de entrada1 a prompt modelo acima
+depois informando ou elevando a constande de entrada1 a parsefloat
+modelo abaixo
+anbas as entradas
+
+const x = parseFloat (entrada1)
+const y = parseFloat(entrada2)
+*/
+const soma = x + y;
+const subtração = x - y;
+const multiplicação = x * y;
+const divisão = x / y;
+
+window.alert(
+  " Resultado das operacões: \n" +
+    "\nSoma: " +
+    soma +
+    "\n Subtração: " +
+    subtração +
+    "\n Multiplicação: " +
+    multiplicação +
+    "\n divisão: " +
+    divisão
+);
+// exercicio poderia ser resolvido com qualquer tipo de variavel
